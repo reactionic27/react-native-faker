7da7ccbfe0a7effe3c62ef4828ce19e9e888350b
reactionic127 <richard.gill127@gmail.com>
2020-12-25 01:10:40 +0800

    clean up package.json

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {Provider} from 'react-redux';
-import {fromJS} from 'immutable';
 import {LoadingSpinner} from './src/components/LoadingSpinner';
 import {Users} from './src/pages/Users';
 import configureStore from './src/redux/store';
 
-const initialState = fromJS({});
-const store = configureStore(initialState);
+const store = configureStore();
 
 export default function App() {
   const [loading, setLoading] = useState(true);
